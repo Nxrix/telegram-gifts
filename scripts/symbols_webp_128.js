d1da1b585a8d153b38b3cb00d3e0e7538cf86de4
Nxrix <nxrixz@gmail.com>
2025-09-01 16:54:31 +0330

    Create symbols_webp_128.js

---
+++ scripts/symbols_webp_128.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const fix_name = (n) => n.replace(/[^a-zA-Z0-9]/g, "");
+const fix_name2 = (n) => fix_name(n).toLowerCase();
+(async()=>{
+  const symbols = await fetch(("https://proxy.thermos.gifts/api/v1/symbols")).json();
+  fs.mkdirSync("./data/symbols/webp/128/", { recursive: true });
+  for (const { name, image_url } of symbols) {
+    if (fs.existsSync(`./data/symbols/webp/128/${fix_name2(name)}.webp`)) {
+      continue;
+    }
+    try {
+      fs.writeFileSync(`./data/symbols/webp/128/${fix_name2(name)}.webp`,Buffer.from((await(await fetch(image_url)).arrayBuffer())));
+    } catch (e) {
+      console.log(name,e.toString());
+    }
+  }
+})();
