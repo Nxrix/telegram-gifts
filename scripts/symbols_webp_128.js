a0b6488506885abfdd86faa5b31a7654f5630709
Nxrix <nxrixz@gmail.com>
2025-09-01 16:58:07 +0330

    Update symbols_webp_128.js

--- scripts/symbols_webp_128.js
+++ scripts/symbols_webp_128.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const fix_name = (n) => n.replace(/[^a-zA-Z0-9]/g, "");
 const fix_name2 = (n) => fix_name(n).toLowerCase();
 (async()=>{
-  const symbols = await fetch(("https://proxy.thermos.gifts/api/v1/symbols")).json();
+  const symbols = (await fetch(("https://proxy.thermos.gifts/api/v1/symbols")).json();
   fs.mkdirSync("./data/symbols/webp/128/", { recursive: true });
   for (const { name, image_url } of symbols) {
     if (fs.existsSync(`./data/symbols/webp/128/${fix_name2(name)}.webp`)) {
