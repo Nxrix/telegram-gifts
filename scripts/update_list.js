6ff783bab87a0e6981e776ec4000bbb3b25d5279
Nxrix <nxrixz@gmail.com>
2025-09-07 17:05:31 +0330

    Update update_list.js

--- scripts/update_list.js
+++ scripts/update_list.js
@@ -1,9 +1,42 @@
 const fs = require("fs");
+
 const fix_name = (n) => n.replace(/[^a-zA-Z0-9]/g, "");
 const fix_name2 = (n) => fix_name(n).toLowerCase();
-const fs = require("fs");
+
+const get_supply = async (n) => {
+  return (await (await fetch("https://t.me/nft/"+n)).text())
+    .split("<td>")[5]
+    ?.split(" issued")[0]
+    ?.split("/")[0]
+    .replaceAll(" ","")|0;
+}
+
+const get_gift = async (n) => {
+  return [
+    n.split("-")[1],
+    ...((await (await fetch("https://t.me/nft/" + n)).text())
+      .match(
+        /<meta\s+name=["']twitter:description["']\s+content=["']([^"']*)["']/i
+      )?.[1]
+      .trim()
+      .split("\n")
+      .map((i) => i.split(": ")[1]) || [])
+    ];
+  };
+}
+
 (async()=>{
   fs.mkdirSync("./data/lists/v1/", { recursive: true });
   const list = [];
-  fs.writeFileSync(`./data/lists/v1/${fix_name(process.env.name)}.json`,JSON.stringify(list));
+  const name = process.env.name;
+  const supply = get_supply(fix_name2(name)+"-1");
+  for (let i=1;i<=supply;i++) {
+    const gift = await get_gift(fix_name2(name)+"-"+i);
+    if (gift) {
+      list.push(gift);
+    } else {
+      console.log(i);
+    }
+  }
+  fs.writeFileSync(`./data/lists/v1/${fix_name(name)}.json`,JSON.stringify(list));
 })();
